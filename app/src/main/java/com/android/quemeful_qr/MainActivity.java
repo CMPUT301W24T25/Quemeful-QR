c7e2581452535f705bd6b3593df20a5cfc9c41b3
Babariyayash <babariyayash9200@gmail.com>
2024-02-21 22:13:35 -0700

    added bottom naviagtion bar

--- app/src/main/java/com/android/quemeful_qr/MainActivity.java
+++ app/src/main/java/com/android/quemeful_qr/MainActivity.java
@@ -1,23 +1,60 @@
 package com.android.quemeful_qr;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
 public class MainActivity extends AppCompatActivity {
 
     private MeowBottomNavigation bottomNavigation;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+        bottomNavigation.add(new MeowBottomNavigation.Model(1, R.drawable.baseline_dashboard_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2, R.drawable.baseline_event_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3, R.drawable.baseline_account_circle_24));
 
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                // your codes
+                Toast.makeText(MainActivity.this,
+                        "clicked item :" + item.getId(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                Fragment fragment = null;
+                switch (item.getId()){
+                    case 1:
+                        fragment = new Home();
+                        break;
+                    case 2:
+                        fragment = new Events();
+                        break;
+                    case 3:
+                        fragment = new Profile();
+                        break;
+                }
+                loadFragment(fragment);
+            }
+        });
+
+        bottomNavigation.show(1, true);
     }
-
-
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
 }
---
+++ app/src/main/java/com/android/quemeful_qr/Profile.java
@@ -0,0 +1,23 @@
+package com.android.quemeful_qr;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Profile extends Fragment {
+
+    public Profile
+            () {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+}
