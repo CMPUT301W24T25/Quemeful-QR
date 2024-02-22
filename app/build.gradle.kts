23ff7c995084b36af71c0fc424c99c7ebfec27f0
Babariyayash <babariyayash9200@gmail.com>
2024-02-21 19:22:55 -0700

    added bottom navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,4 +36,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.etebarian:meow-bottom-navigation-java:1.2.0")
+    implementation("androidx.navigation:navigation-fragment:2.7.6")
+    implementation("com.google.firebase:firebase-firestore:23.0.3")
 }
--- app/src/main/java/com/android/quemeful_qr/MainActivity.java
+++ app/src/main/java/com/android/quemeful_qr/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MeowBottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
+
+
 }
