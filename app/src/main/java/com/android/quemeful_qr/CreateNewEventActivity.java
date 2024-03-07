e108404343b991f99b54a0f3880bc6e935523607
Victoria Trac <vtrac1@ualberta.ca>
2024-03-07 03:36:09 -0700

    start event test

--- app/src/main/java/com/android/quemeful_qr/CreateNewEventActivity.java
+++ app/src/main/java/com/android/quemeful_qr/CreateNewEventActivity.java
@@ -47,14 +47,9 @@ import java.util.UUID;
  * https://www.youtube.com/watch?v=cxEb4IafzZU
  * https://developer.android.com/training/data-storage/shared/photopicker
  */
+
 public class CreateNewEventActivity extends AppCompatActivity {
-    // declare variables
-//    private TextInputEditText eventTitle;
-//    private TextInputEditText eventDescription;
-//    private TextInputEditText startDate;
-//    private TextInputEditText startTime;
-//    private TextInputEditText endDate;
-//    private TextInputEditText endTime;
+    // xml variables
     private EditText eventTitle;
     private EditText eventDescription;
     private EditText startDate;
@@ -65,12 +60,14 @@ public class CreateNewEventActivity extends AppCompatActivity {
     private Button cancelButton;
     private Button createButton;
     private ImageButton uploadPoster;
-    //for date and time picker
-//    Calendar calendar = Calendar.getInstance();
-//    Calendar time = Calendar.getInstance();
+
+    // poster
     private Uri selectedImageUri;
+
+    //firebase
     private FirebaseFirestore db;
     private CollectionReference eventsRef;
+    // attributes for event class
     private String eventUUID;
     private String parsedTime;
     private String parsedDate;
@@ -80,7 +77,7 @@ public class CreateNewEventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
 
-        // initialize variables
+        // from xml
         eventTitle = findViewById(R.id.enter_title);
         eventDescription = findViewById(R.id.enter_event_details);
         startDate = findViewById(R.id.enter_startDate);
@@ -91,31 +88,12 @@ public class CreateNewEventActivity extends AppCompatActivity {
         cancelButton = findViewById(R.id.cancel_button);
         createButton = findViewById(R.id.create_button);
         uploadPoster = findViewById(R.id.add_poster_button);
+
+        //initialize firebase
         db = FirebaseFirestore.getInstance();
         eventsRef = db.collection("events");
-        // for adding poster by user added READ_MEDIA_IMAGES permission in AndroidManifest.xml
-        // start image selecting activity and get its result using ActivityResultLauncher
-//        ActivityResultLauncher<PickVisualMediaRequest> pickImage = registerForActivityResult(
-//                new ActivityResultContracts.PickVisualMedia(),
-//                uri -> {
-//                    if (uri != null) {
-//                        // display the selected image
-//                        uploadPoster.setImageURI(uri);
-//                    }
-//                }
-//        );
 
-        // initiate selection of image when the add button is clicked using Intent
-        // add poster
-//        uploadPoster.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // allow user to select image from gallery
-//                pickImage.launch(new PickVisualMediaRequest.Builder()
-//                        .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
-//                        .build());
-//            }
-//        });
+        //displays selected pic on app
         uploadPoster.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -123,27 +101,35 @@ public class CreateNewEventActivity extends AppCompatActivity {
                 imageChooser();
             }
         });
-
+        //creates event
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //generates random id which is the QR code
                 eventUUID = UUID.randomUUID().toString();
+
                 String eventName = eventTitle.getText().toString();
                 String eventLocation = "location";
-
-
                 String eventTime = startTime.getText().toString();
                 String eventDate = startDate.getText().toString();
+
+                //formats time for display
                 parsedTime = DateUtils.formatTime(eventTime);
                 Log.d("check time", parsedTime);
+
+                //formats date for display
                 parsedDate = DateUtils.formatDate(eventDate);
+
                 String eventDescr = eventDescription.getText().toString();
                 try {
+                    // converts uri to bitmap
                     Bitmap bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), selectedImageUri);
+                    // converts bitmap to base64 string
                     ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
                     // In case you want to compress your image, here it's at 40%
                     bitmap.compress(Bitmap.CompressFormat.JPEG, 40, byteArrayOutputStream);
                     byte[] byteArray = byteArrayOutputStream.toByteArray();
+                    //create new event
                     EventHelper event = new EventHelper(eventUUID, eventName, eventLocation, parsedTime, parsedDate, eventDescr, Base64.encodeToString(byteArray, Base64.DEFAULT));
                     addNewEvent(event);
                 } catch (IOException e) {
@@ -153,31 +139,14 @@ public class CreateNewEventActivity extends AppCompatActivity {
               }
         });
 
-        // set listener on create button
-//        createButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                // retrieve user input for event title and details
-//                String title = eventTitle.getText().toString();
-//                String description = eventDescription.getText().toString();
-//
-//
-//
-//                // set the retrieved details in their respective text boxes
-//                eventTitle.setText(title);
-//                eventDescription.setText(description);
-//                // call selectDate and selectTime
-//                selectDate();
-//                selectTime();
-//                Toast.makeText(CreateNewEventActivity.this, "New Event successfully created", Toast.LENGTH_SHORT).show();
-//            }
-//        });
 
         generateQRButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(CreateNewEventActivity.this, GenerateNewQRActivity.class);
+               
                 startActivity(intent);
+
             }
         });
 
@@ -195,35 +164,7 @@ public class CreateNewEventActivity extends AppCompatActivity {
      * selectDate() allows user to pick date.
      * setDateFormat() is a method used to set the format for displaying the date in dd/MM/yyyy.
      */
-//    private void selectDate(){
-//        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
-//            @Override
-//            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-//                calendar.set(Calendar.YEAR, year);
-//                calendar.set(Calendar.MONTH, month);
-//                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-//
-//                // set the date in its fields
-//                startDate.setText(setDateFormat());
-//                endDate.setText(setDateFormat());
-//            }
-//        };
 
-        // start date picker and end date picker works the same way
-//        startDate.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                new DatePickerDialog(CreateNewEventActivity.this, date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
-//            }
-//        });
-//
-//        endDate.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                new DatePickerDialog(CreateNewEventActivity.this, date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
-//            }
-//        });
-//    }
     private void addNewEvent(EventHelper event) {
 
         String eventName = eventTitle.getText().toString();
@@ -284,48 +225,5 @@ public class CreateNewEventActivity extends AppCompatActivity {
             });
 
 
-//    private String setDateFormat(){
-//        String format = "dd/MM/yyyy";
-//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format, Locale.CANADA);
-//        return simpleDateFormat.format(calendar.getTime());
-//    }
 
-    /**
-     * This method is to get the start and the end time from user.
-     * selectTime() allows user to pick time.
-     * setTimeFormat() is a method used to set the format for displaying the time in HH:mm.
-     */
-//    private void selectTime(){
-//        int hours = time.get(Calendar.HOUR_OF_DAY);
-//        int minutes = time.get(Calendar.MINUTE);
-//        TimePickerDialog.OnTimeSetListener timePickerDialog = new TimePickerDialog.OnTimeSetListener() {
-//            @Override
-//            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-//                time.set(Calendar.HOUR_OF_DAY, hours);
-//                time.set(Calendar.MINUTE, minutes);
-//
-//                // set the times in their fields
-//                startTime.setText(setTimeFormat());
-//                endTime.setText(setTimeFormat());
-//            }
-//        };
-
-//        startTime.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                new TimePickerDialog(CreateNewEventActivity.this, (TimePickerDialog.OnTimeSetListener) time, hours, minutes, true).show();
-//            }
-//        });
-//        endTime.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                new TimePickerDialog(CreateNewEventActivity.this, (TimePickerDialog.OnTimeSetListener) time, hours, minutes, true).show();
-//            }
-//        });
-//    }
-//    private String setTimeFormat(){
-//        String format = "HH:mm";
-//        SimpleDateFormat timeFormat = new SimpleDateFormat(format, Locale.CANADA);
-//        return timeFormat.format(time.getTime());
-//    }
 }
--- app/src/test/java/com/android/quemeful_qr/EventClassTest.java
+++
@@ -1,5 +0,0 @@
-package com.android.quemeful_qr;
-public class EventClassTest {
-    private EventHelper mockEvent(){ return new EventHelper("id", "title","location","time","date","description","poster");}
-
-}
