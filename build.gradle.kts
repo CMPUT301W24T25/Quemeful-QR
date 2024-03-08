67e4c5133f803bbdf17466c04252d09354df10e0
Endem14 <mmerchan@Ualberta.ca>
2024-03-08 15:34:19 -0700

    fixed merge issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,13 +8,5 @@ buildscript {
 
 plugins {
     id("com.android.application") version "8.2.2" apply false
-<<<<<<< HEAD
-<<<<<<< HEAD
     id("com.google.gms.google-services") version "4.4.1" apply false
-=======
-    id("com.google.gms.google-services") version "4.4.0" apply false
->>>>>>> ebe18dae8eade760676aa43dc4e74714d486f381
-=======
-    id("com.google.gms.google-services") version "4.4.1" apply false
->>>>>>> Hussain_TestCases
 }
