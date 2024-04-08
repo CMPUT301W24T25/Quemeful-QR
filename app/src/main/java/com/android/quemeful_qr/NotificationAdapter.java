5c3e6ea465f9871da2fd35f92e4573bc50a536e0
Victoria Trac <vtrac1@ualberta.ca>
2024-04-08 13:37:47 -0600

    notification change base 64 string to firebase link for images

--- app/src/main/java/com/android/quemeful_qr/NotificationAdapter.java
+++ app/src/main/java/com/android/quemeful_qr/NotificationAdapter.java
@@ -17,6 +17,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
 import java.util.List;
 
 public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder> {
@@ -75,9 +77,14 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
             fromTextView.setText(  notification.getFrom() );
 
             if (notification.getImage() != null && !notification.getImage().trim().isEmpty()) {
-                byte[] decodedString = Base64.decode(notification.getImage().trim(), Base64.DEFAULT);
-                Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-                posterImageView.setImageBitmap(decodedByte);
+//                byte[] decodedString = Base64.decode(notification.getImage().trim(), Base64.DEFAULT);
+//                Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+//                posterImageView.setImageBitmap(decodedByte);
+                Glide.with(itemView.getContext())
+                        .load(notification.getImage()) // Load the image directly from the URI
+                        .placeholder(R.drawable.gradient_background) // Optional: a placeholder until the image loads
+                        .error(R.drawable.gradient_background) // Optional: an error image if the load fails
+                        .into(posterImageView);
             } else {
                 posterImageView.setImageResource(R.drawable.gradient_background); // Placeholder if no image is present
             }
